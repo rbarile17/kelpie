0ad0fe1051d0314f59ee422d6283989c7e96b706
rbarile17 <roberto.barile98@gmail.com>
2023-05-30 12:39:41 +0200

    :recycle::shirt: Improve run script
    Readability and style improvements for run script

--- src/explain.py
+++ src/explain.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import random
 import time
 
@@ -26,322 +27,279 @@ from .link_prediction.models import (
     EPOCHS,
     BATCH_SIZE,
     REGULARIZER_NAME,
-    DIMENSION, 
-    INIT_SCALE
+    DIMENSION,
+    INIT_SCALE,
 )
 
-datasets = ALL_DATASET_NAMES
 
-parser = argparse.ArgumentParser(
-    description="Model-agnostic tool for explaining link predictions"
-)
+def parse_args():
+    datasets = ALL_DATASET_NAMES
 
-parser.add_argument(
-    "--dataset", choices=datasets, help="Dataset in {}".format(datasets), required=True
-)
-
-optimizers = ["Adagrad", "Adam", "SGD"]
-parser.add_argument(
-    "--optimizer",
-    choices=optimizers,
-    default="Adagrad",
-    help="Optimizer in {} to use in post-training".format(optimizers),
-)
-
-parser.add_argument(
-    "--batch_size", default=100, type=int, help="Batch size to use in post-training"
-)
-
-parser.add_argument(
-    "--max_epochs",
-    default=200,
-    type=int,
-    help="Number of epochs to run in post-training",
-)
-
-parser.add_argument("--dimension", default=1000, type=int, help="Factorization rank.")
-
-parser.add_argument("--learning_rate", default=1e-1, type=float, help="Learning rate")
-
-parser.add_argument("--reg", default=0, type=float, help="Regularization weight")
-
-parser.add_argument("--init", default=1e-3, type=float, help="Initial scale")
-
-parser.add_argument(
-    "--decay1",
-    default=0.9,
-    type=float,
-    help="Decay rate for the first moment estimate in Adam",
-)
-
-parser.add_argument(
-    "--decay2",
-    default=0.999,
-    type=float,
-    help="Decay rate for second moment estimate in Adam",
-)
-
-parser.add_argument(
-    "--model_path",
-    help="Path to the model to explain the predictions of",
-    required=True,
-)
-
-parser.add_argument(
-    "--facts_to_explain_path",
-    type=str,
-    required=True,
-    help="path of the file with the facts to explain the predictions of.",
-)
-
-parser.add_argument(
-    "--coverage",
-    type=int,
-    default=10,
-    help="Number of random entities to extract and convert",
-)
-
-parser.add_argument(
-    "--baseline",
-    type=str,
-    default=None,
-    choices=[None, "k1", "data_poisoning", "criage"],
-    help="attribute to use when we want to use a baseline rather than the Kelpie engine",
-)
-
-parser.add_argument(
-    "--entities_to_convert",
-    type=str,
-    help="path of the file with the entities to convert (only used by baselines)",
-)
-
-parser.add_argument(
-    "--mode",
-    type=str,
-    default="sufficient",
-    choices=["sufficient", "necessary"],
-    help="The explanation mode",
-)
-
-parser.add_argument(
-    "--relevance_threshold",
-    type=float,
-    default=None,
-    help="The relevance acceptance threshold to use",
-)
-
-prefilters = [
-    TOPOLOGY_PREFILTER,
-    TYPE_PREFILTER,
-    NO_PREFILTER,
-    WEIGHTED_TOPOLOGY_PREFILTER,
-]
-parser.add_argument(
-    "--prefilter",
-    choices=prefilters,
-    default="graph-based",
-    help="Prefilter type in {} to use in pre-filtering".format(prefilters),
-)
-
-parser.add_argument(
-    "--prefilter_threshold",
-    type=int,
-    default=20,
-    help="The number of promising training facts to keep after prefiltering",
-)
-
-args = parser.parse_args()
-
-########## LOAD DATASET
-
-# deterministic!
-seed = 42
-torch.backends.cudnn.deterministic = True
-numpy.random.seed(seed)
-torch.manual_seed(seed)
-random.seed(seed)
-torch.cuda.set_rng_state(torch.cuda.get_rng_state())
-
-hyperparameters = {
-    DIMENSION: args.dimension,
-    INIT_SCALE: args.init,
-    LEARNING_RATE: args.learning_rate,
-    OPTIMIZER_NAME: args.optimizer,
-    DECAY_1: args.decay1,
-    DECAY_2: args.decay2,
-    REGULARIZER_WEIGHT: args.reg,
-    EPOCHS: args.max_epochs,
-    BATCH_SIZE: args.batch_size,
-    REGULARIZER_NAME: "N3",
-}
-
-prefilter = args.prefilter
-relevance_threshold = args.relevance_threshold
-
-# load the dataset and its training triples
-print("Loading dataset %s..." % args.dataset)
-dataset = Dataset(dataset=args.dataset)
-
-print("Reading facts to explain...")
-with open(args.facts_to_explain_path, "r") as facts_file:
-    testing_facts = [x.strip().split("\t") for x in facts_file.readlines()]
-
-model = ComplEx(
-    dataset=dataset, hyperparameters=hyperparameters, init_random=True
-)  # type: ComplEx
-model.to("cuda")
-model.load_state_dict(torch.load(args.model_path))
-model.eval()
-
-start_time = time.time()
-
-if args.baseline is None:
-    kelpie = Kelpie(
-        model=model,
-        dataset=dataset,
-        hyperparameters=hyperparameters,
-        prefilter_type=prefilter,
-        relevance_threshold=relevance_threshold,
-    )
-elif args.baseline == "data_poisoning":
-    kelpie = DataPoisoning(
-        model=model,
-        dataset=dataset,
-        hyperparameters=hyperparameters,
-        prefilter_type=prefilter,
-    )
-elif args.baseline == "criage":
-    kelpie = Criage(model=model, dataset=dataset, hyperparameters=hyperparameters)
-elif args.baseline == "k1":
-    kelpie = Kelpie(
-        model=model,
-        dataset=dataset,
-        hyperparameters=hyperparameters,
-        prefilter_type=prefilter,
-        relevance_threshold=relevance_threshold,
-        max_explanation_length=1,
-    )
-else:
-    kelpie = Kelpie(
-        model=model,
-        dataset=dataset,
-        hyperparameters=hyperparameters,
-        prefilter_type=prefilter,
-        relevance_threshold=relevance_threshold,
+    parser = argparse.ArgumentParser(
+        description="Model-agnostic tool for explaining link predictions"
     )
 
-testing_fact_2_entities_to_convert = None
-if args.mode == "sufficient" and args.entities_to_convert is not None:
-    print("Reading entities to convert...")
-    testing_fact_2_entities_to_convert = {}
-    with open(args.entities_to_convert, "r") as entities_to_convert_file:
-        entities_to_convert_lines = entities_to_convert_file.readlines()
-        i = 0
-        while i < len(entities_to_convert_lines):
-            cur_head, cur_rel, cur_name = (
-                entities_to_convert_lines[i].strip().split(";")
+    parser.add_argument(
+        "--dataset",
+        choices=datasets,
+        help="Dataset in {}".format(datasets),
+        required=True,
+    )
+
+    optimizers = ["Adagrad", "Adam", "SGD"]
+    parser.add_argument(
+        "--optimizer",
+        choices=optimizers,
+        default="Adagrad",
+        help="Optimizer in {} to use in post-training".format(optimizers),
+    )
+
+    parser.add_argument(
+        "--batch_size", default=100, type=int, help="Batch size to use in post-training"
+    )
+
+    parser.add_argument(
+        "--max_epochs",
+        default=200,
+        type=int,
+        help="Number of epochs to run in post-training",
+    )
+
+    parser.add_argument(
+        "--dimension", default=1000, type=int, help="Factorization rank."
+    )
+
+    parser.add_argument(
+        "--learning_rate", default=1e-1, type=float, help="Learning rate"
+    )
+
+    parser.add_argument("--reg", default=0, type=float, help="Regularization weight")
+
+    parser.add_argument("--init", default=1e-3, type=float, help="Initial scale")
+
+    parser.add_argument(
+        "--decay1",
+        default=0.9,
+        type=float,
+        help="Decay rate for the first moment estimate in Adam",
+    )
+
+    parser.add_argument(
+        "--decay2",
+        default=0.999,
+        type=float,
+        help="Decay rate for second moment estimate in Adam",
+    )
+
+    parser.add_argument(
+        "--model_path",
+        help="Path to the model to explain the predictions of",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--triples_to_explain",
+        type=str,
+        required=True,
+        help="path of the file with the facts to explain the predictions of.",
+    )
+
+    parser.add_argument(
+        "--coverage",
+        type=int,
+        default=10,
+        help="Number of random entities to extract and convert",
+    )
+
+    parser.add_argument(
+        "--baseline",
+        type=str,
+        default=None,
+        choices=[None, "k1", "data_poisoning", "criage"],
+        help="attribute to use when we want to use a baseline rather than the Kelpie engine",
+    )
+
+    parser.add_argument(
+        "--entities_to_convert",
+        type=str,
+        help="path of the file with the entities to convert (only used by baselines)",
+    )
+
+    parser.add_argument(
+        "--mode",
+        type=str,
+        default="sufficient",
+        choices=["sufficient", "necessary"],
+        help="The explanation mode",
+    )
+
+    parser.add_argument(
+        "--relevance_threshold",
+        type=float,
+        default=None,
+        help="The relevance acceptance threshold to use",
+    )
+
+    prefilters = [
+        TOPOLOGY_PREFILTER,
+        TYPE_PREFILTER,
+        NO_PREFILTER,
+        WEIGHTED_TOPOLOGY_PREFILTER,
+    ]
+    parser.add_argument(
+        "--prefilter",
+        choices=prefilters,
+        default="graph-based",
+        help="Prefilter type in {} to use in pre-filtering".format(prefilters),
+    )
+
+    parser.add_argument(
+        "--prefilter_threshold",
+        type=int,
+        default=20,
+        help="The number of promising training facts to keep after prefiltering",
+    )
+
+    return parser.parse_args()
+
+
+def main(args):
+    seed = 42
+    torch.backends.cudnn.deterministic = True
+    numpy.random.seed(seed)
+    torch.manual_seed(seed)
+    random.seed(seed)
+    torch.cuda.set_rng_state(torch.cuda.get_rng_state())
+
+    hyperparameters = {
+        DIMENSION: args.dimension,
+        INIT_SCALE: args.init,
+        LEARNING_RATE: args.learning_rate,
+        OPTIMIZER_NAME: args.optimizer,
+        DECAY_1: args.decay1,
+        DECAY_2: args.decay2,
+        REGULARIZER_WEIGHT: args.reg,
+        EPOCHS: args.max_epochs,
+        BATCH_SIZE: args.batch_size,
+        REGULARIZER_NAME: "N3",
+    }
+
+    prefilter = args.prefilter
+    relevance_threshold = args.relevance_threshold
+
+    print(f"Loading dataset {args.dataset}...")
+    dataset = Dataset(dataset=args.dataset)
+
+    print("Reading triples to explain...")
+    with open(args.triples_to_explain, "r") as triples_file:
+        triples_to_explain = [x.strip().split("\t") for x in triples_file.readlines()]
+
+    model = ComplEx(dataset=dataset, hyperparameters=hyperparameters, init_random=True)
+    model.to("cuda")
+    model.load_state_dict(torch.load(args.model_path))
+    model.eval()
+
+    start_time = time.time()
+
+    if args.baseline is None:
+        kelpie = Kelpie(
+            model=model,
+            dataset=dataset,
+            hyperparameters=hyperparameters,
+            prefilter_type=prefilter,
+            relevance_threshold=relevance_threshold,
+        )
+    elif args.baseline == "data_poisoning":
+        kelpie = DataPoisoning(
+            model=model,
+            dataset=dataset,
+            hyperparameters=hyperparameters,
+            prefilter_type=prefilter,
+        )
+    elif args.baseline == "criage":
+        kelpie = Criage(model=model, dataset=dataset, hyperparameters=hyperparameters)
+    elif args.baseline == "k1":
+        kelpie = Kelpie(
+            model=model,
+            dataset=dataset,
+            hyperparameters=hyperparameters,
+            prefilter_type=prefilter,
+            relevance_threshold=relevance_threshold,
+            max_explanation_length=1,
+        )
+    else:
+        kelpie = Kelpie(
+            model=model,
+            dataset=dataset,
+            hyperparameters=hyperparameters,
+            prefilter_type=prefilter,
+            relevance_threshold=relevance_threshold,
+        )
+
+    explanations = []
+    for i, triple in enumerate(triples_to_explain):
+        head, relation, tail = triple
+        print(
+            f"\nExplaining triple {i + 1} on {len(triples_to_explain)}: " \
+            f"<{head},{relation},{tail}>"
+        )
+        triple = dataset.ids_triple(triple)
+
+        if args.mode == "sufficient":
+            (
+                rule_to_relevance,
+                entities_to_convert,
+            ) = kelpie.explain_sufficient(
+                triple_to_explain=triple,
+                perspective="head",
+                num_promising_triples=args.prefilter_threshold,
+                num_entities_to_convert=args.coverage,
+                entities_to_convert=None,
             )
-            assert [cur_head, cur_rel, cur_name] in testing_facts
-            cur_entities_to_convert = (
-                entities_to_convert_lines[i + 1].strip().split(",")
+
+            if entities_to_convert is None or len(entities_to_convert) == 0:
+                continue
+            entities_to_convert = [dataset.id_to_entity[x] for x in entities_to_convert]
+
+            rule_to_relevance = [
+                (
+                    [dataset.labels_triple(rule_triple) for rule_triple in rule],
+                    relevance,
+                )
+                for rule, relevance in rule_to_relevance
+            ]
+
+            explanations.append(
+                {
+                    "triple": dataset.labels_triple(triple),
+                    "entities_to_convert": entities_to_convert,
+                    "rule_to_relevance": rule_to_relevance,
+                }
             )
-            testing_fact_2_entities_to_convert[
-                (cur_head, cur_rel, cur_name)
-            ] = cur_entities_to_convert
-            i += 3
-
-output_lines = []
-for i, fact in enumerate(testing_facts):
-    head, relation, tail = fact
-    print(
-        "Explaining fact "
-        + str(i)
-        + " on "
-        + str(len(testing_facts))
-        + ": <"
-        + head
-        + ","
-        + relation
-        + ","
-        + tail
-        + ">"
-    )
-    head_id, relation_id, tail_id = (
-        dataset.entity_to_id[head],
-        dataset.relation_to_id[relation],
-        dataset.entity_to_id[tail],
-    )
-    triple_to_explain = (head_id, relation_id, tail_id)
-
-    if args.mode == "sufficient":
-        entities_to_convert_ids = (
-            None
-            if testing_fact_2_entities_to_convert is None
-            else [
-                dataset.entity_to_id[x]
-                for x in testing_fact_2_entities_to_convert[(head, relation, tail)]
+        elif args.mode == "necessary":
+            rule_to_relevance = kelpie.explain_necessary(
+                triple_to_explain=triple,
+                perspective="head",
+                num_promising_triples=args.prefilter_threshold,
+            )
+            rule_to_relevance = [
+                (
+                    [dataset.labels_triple(rule_triple) for rule_triple in rule],
+                    relevance,
+                )
+                for rule, relevance in rule_to_relevance
             ]
-        )
+            explanations.append(
+                {
+                    "triple": dataset.labels_triple(triple),
+                    "rule_to_relevance": rule_to_relevance,
+                }
+            )
 
-        (
-            rule_triples_with_relevance,
-            entities_to_convert_ids,
-        ) = kelpie.explain_sufficient(
-            triple_to_explain=triple_to_explain,
-            perspective="head",
-            num_promising_triples=args.prefilter_threshold,
-            num_entities_to_convert=args.coverage,
-            entities_to_convert=entities_to_convert_ids,
-        )
+    print("Required time: " + str(time.time() - start_time) + " seconds")
+    with open("output.json", "w") as output:
+        json.dump(explanations, output)
 
-        if entities_to_convert_ids is None or len(entities_to_convert_ids) == 0:
-            continue
-        entities_to_convert = [
-            dataset.id_to_entity[x] for x in entities_to_convert_ids
-        ]
 
-        rule_facts_with_relevance = []
-        for cur_rule_with_relevance in rule_triples_with_relevance:
-            cur_rule_triples, cur_relevance = cur_rule_with_relevance
-
-            cur_rule_facts = [
-                dataset.labels_triple(triple) for triple in cur_rule_triples
-            ]
-            cur_rule_facts = ";".join([";".join(x) for x in cur_rule_facts])
-            rule_facts_with_relevance.append(cur_rule_facts + "::" + str(cur_relevance))
-
-        print(";".join(fact))
-        print(" | ".join(entities_to_convert))
-        print(" | ".join(rule_facts_with_relevance))
-        print()
-        output_lines.append(";".join(fact) + "\n")
-        output_lines.append("|".join(entities_to_convert) + "\n")
-        output_lines.append("|".join(rule_facts_with_relevance) + "\n")
-        output_lines.append("\n")
-
-    elif args.mode == "necessary":
-        rule_triples_with_relevance = kelpie.explain_necessary(
-            triple_to_explain=triple_to_explain,
-            perspective="head",
-            num_promising_triples=args.prefilter_threshold,
-        )
-        rule_facts_with_relevance = []
-        for cur_rule_with_relevance in rule_triples_with_relevance:
-            cur_rule_triples, cur_relevance = cur_rule_with_relevance
-
-            cur_rule_facts = [
-                dataset.labels_triple(triple) for triple in cur_rule_triples
-            ]
-            cur_rule_facts = ";".join([";".join(x) for x in cur_rule_facts])
-            rule_facts_with_relevance.append(cur_rule_facts + "::" + str(cur_relevance))
-        print(";".join(fact))
-        print("| ".join(rule_facts_with_relevance))
-        print()
-        output_lines.append(";".join(fact) + "\n")
-        output_lines.append("|".join(rule_facts_with_relevance) + "\n")
-        output_lines.append("\n")
-
-end_time = time.time()
-print("Required time: " + str(end_time - start_time) + " seconds")
-with open("output.txt", "w") as output:
-    output.writelines(output_lines)
+if __name__ == "__main__":
+    main(parse_args())
