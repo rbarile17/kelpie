a5f26605ceca37723d667be2377ef84cbc6b65b6
rbarile17 <roberto.barile98@gmail.com>
2023-06-06 16:33:11 +0200

    :sparkles: Implement semantic similarity module
    The module contains a function to compute the average pairwise semantic
    similarity of a group of triples

--- src/semantic/__init__.py
+++ src/semantic/__init__.py
@@ -3,6 +3,10 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 from .. import DBPEDIA50_PATH, DBPEDIA50_REASONED_PATH
 
 from ..data import Dataset
+from .semantic_similarity import (
+    compute_semantic_similarity_entities,
+    compute_semantic_similarity_triples,
+)
 
 dataset = Dataset(dataset="DBpedia50")
 
